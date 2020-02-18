88c9f4f7e8e383d8df203ca4ad7db9f180a3ef5d
Johan Björk <capellidipaja@gmail.com>
2020-02-18 12:18:20 +0100

    Compiled with 8.0 (LD=10.2 but it should work)

--- lib/make.sh
+++ lib/make.sh
@@ -6,7 +6,7 @@ cd roi_pooling/src/cuda
 
 echo "Compiling roi pooling kernels by nvcc..."
 nvcc -c -o roi_pooling_kernel.cu.o roi_pooling_kernel.cu \
-	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC # TODO: -arch=sm_75
+	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC # TODO -arch=sm_75
 
 cd ../../
 python build.py
