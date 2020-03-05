b52bb22ebbac33ccb36d092dcc18e767bb7c9ca9
FireSterLine <24519853+FireSterLine@users.noreply.github.com>
2020-03-05 12:41:01 +0100

    Little Fix

--- lib/nets/vrd_model.py
+++ lib/nets/vrd_model.py
@@ -6,33 +6,35 @@ import torch.nn as nn
 import sys
 import os.path as osp
 
-from lib.network import FC
+from lib.network import FC, Conv2d
 
 class vrd_model(nn.Module):
     def __init__(self, args):
         super(vrd_model, self).__init__()
 
+        self.self.use_bn = False
+        
         self.n_obj  = args.n_obj
         self.n_pred = args.n_pred
 
-        self.conv1 = nn.Sequential(Conv2d(3, 64, 3, same_padding=True, bn=bn),
-                                   Conv2d(64, 64, 3, same_padding=True, bn=bn),
+        self.conv1 = nn.Sequential(Conv2d(3, 64, 3, same_padding=True, bn=self.use_bn),
+                                   Conv2d(64, 64, 3, same_padding=True, bn=self.use_bn),
                                    nn.MaxPool2d(2))
-        self.conv2 = nn.Sequential(Conv2d(64, 128, 3, same_padding=True, bn=bn),
-                                   Conv2d(128, 128, 3, same_padding=True, bn=bn),
+        self.conv2 = nn.Sequential(Conv2d(64, 128, 3, same_padding=True, bn=self.use_bn),
+                                   Conv2d(128, 128, 3, same_padding=True, bn=self.use_bn),
                                    nn.MaxPool2d(2))
 
-        self.conv3 = nn.Sequential(Conv2d(128, 256, 3, same_padding=True, bn=bn),
-                                   Conv2d(256, 256, 3, same_padding=True, bn=bn),
-                                   Conv2d(256, 256, 3, same_padding=True, bn=bn),
+        self.conv3 = nn.Sequential(Conv2d(128, 256, 3, same_padding=True, bn=self.use_bn),
+                                   Conv2d(256, 256, 3, same_padding=True, bn=self.use_bn),
+                                   Conv2d(256, 256, 3, same_padding=True, bn=self.use_bn),
                                    nn.MaxPool2d(2))
-        self.conv4 = nn.Sequential(Conv2d(256, 512, 3, same_padding=True, bn=bn),
-                                   Conv2d(512, 512, 3, same_padding=True, bn=bn),
-                                   Conv2d(512, 512, 3, same_padding=True, bn=bn),
+        self.conv4 = nn.Sequential(Conv2d(256, 512, 3, same_padding=True, bn=self.use_bn),
+                                   Conv2d(512, 512, 3, same_padding=True, bn=self.use_bn),
+                                   Conv2d(512, 512, 3, same_padding=True, bn=self.use_bn),
                                    nn.MaxPool2d(2))
-        self.conv5 = nn.Sequential(Conv2d(512, 512, 3, same_padding=True, bn=bn),
-                                   Conv2d(512, 512, 3, same_padding=True, bn=bn),
-                                   Conv2d(512, 512, 3, same_padding=True, bn=bn))
+        self.conv5 = nn.Sequential(Conv2d(512, 512, 3, same_padding=True, bn=self.use_bn),
+                                   Conv2d(512, 512, 3, same_padding=True, bn=self.use_bn),
+                                   Conv2d(512, 512, 3, same_padding=True, bn=self.use_bn))
 
 
         self.fc_spatial  = FC(8, 256)
--- lib/network.py
+++ lib/network.py
@@ -26,7 +26,7 @@ class Conv2d(nn.Module):
     padding = int((kernel_size - 1) / 2) if same_padding else 0
 
     self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=padding)
-    self.bn = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0, affine=True) if bn else None
+    self.bn   = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0, affine=True) if bn else None
     self.relu = nn.ReLU(inplace=True) if relu else None
 
   def forward(self, x):
