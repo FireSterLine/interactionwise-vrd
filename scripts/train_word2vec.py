7459536dd02e63f32ffdf3f41b73f532931beb12
azfar-imtiaz <azfar.imtiaz@live.com>
2020-04-28 01:17:16 +0200

    Reorder print statements for removing prev checkpoint and saving current checkpoint

--- scripts/train_word2vec.py
+++ scripts/train_word2vec.py
@@ -220,7 +220,6 @@ class EpochSaver(CallbackAny2Vec):
         self.epoch = 1
 
     def on_epoch_end(self, model):
-        print("Saving checkpoint {}".format(self.epoch))
         # remove previously saved checkpoint for storage saving purposes
         prev_checkpoint = os.path.join(self.path_prefix, "epoch_{}_dim_{}.model".format(self.epoch - 1, self.dim))
         prev_checkpoint_vectors = os.path.join(self.path_prefix,
@@ -237,6 +236,7 @@ class EpochSaver(CallbackAny2Vec):
                 os.remove(prev_checkpoint_trainable)
 
         # save current epoch checkpoint to disk
+        print("Saving checkpoint {}".format(self.epoch))
         output_path = get_tmpfile(os.path.join(self.path_prefix, "epoch_{}_dim_{}.model".format(self.epoch, self.dim)))
         model.save(output_path)
         self.epoch += 1
