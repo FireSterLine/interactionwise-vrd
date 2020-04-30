95cea37eb46bf474a9da05682814b22fd65a0c42
FireSterLine <24519853+FireSterLine@users.noreply.github.com>
2020-04-30 19:07:22 +0200

    removed only_sem_subdiff (inefficient)

--- vrd_trainer.py
+++ vrd_trainer.py
@@ -470,7 +470,7 @@ if __name__ == "__main__":
               for pred_sem_mode_1 in [-1, 16]: # 11 #, 16+4, 16+2 , 16+4+1, 16+16+2, 16+16+4+2]: #, 9 16+16, 16+16+4
                 for loss in ["mlab"]: # "bcel"]: # mlab_mse
                   for dataset in ["vrd"]: # , "vg"]:
-                    for prof in ["only_sem_subdiff", "only_sem_subdot", "only_sem_catdiff", "only_sem_catdot", "only_sem_diffdot"]: # ["only_spat", "spat_sem", "only_sem", False]: # , "vg"]:
+                    for prof in ["only_sem_subdot", "only_sem_catdiff", "only_sem_catdot", "only_sem_diffdot"]: # ["only_spat", "spat_sem", "only_sem", False]: # , "vg"]:
                       if "mse" in loss and (pred_sem_mode_1 == -1 or pred_sem_mode_1>=16):
                         continue
                       pred_sem_mode = pred_sem_mode_1+1
